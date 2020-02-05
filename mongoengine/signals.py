e74310870590ae5a35488f7b845753ac9471ecc4
Miral <miral@rippling.com>
2020-02-05 14:15:12 +0530

    signals.py file updated

--- mongoengine/signals.py
+++ mongoengine/signals.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-
-__all__ = ['pre_init', 'post_init', 'pre_save', 'pre_save_post_validation',
-           'post_save', 'pre_delete', 'post_delete']
+__all__ = (
+    "pre_init",
+    "post_init",
+    "pre_save",
+    "pre_save_post_validation",
+    "post_save",
+    "pre_delete",
+    "post_delete",
+)
 
 signals_available = False
 try:
@@ -9,6 +14,7 @@ try:
 
     signals_available = True
 except ImportError:
+
     class Namespace(object):
         def signal(self, name, doc=None):
             return _FakeSignal(name, doc)
@@ -25,26 +31,30 @@ except ImportError:
             self.__doc__ = doc
 
         def _fail(self, *args, **kwargs):
-            raise RuntimeError('signalling support is unavailable '
-                               'because the blinker library is '
-                               'not installed.')
+            raise RuntimeError(
+                "signalling support is unavailable "
+                "because the blinker library is "
+                "not installed."
+            )
 
-        send = lambda *a, **kw: None
-        connect = disconnect = has_receivers_for = receivers_for = \
-            temporarily_connected_to = _fail
+        send = lambda *a, **kw: None  # noqa
+        connect = (
+            disconnect
+        ) = has_receivers_for = receivers_for = temporarily_connected_to = _fail
         del _fail
+
 
 # the namespace for code signals.  If you are not mongoengine code, do
 # not put signals in here.  Create your own namespace instead.
 _signals = Namespace()
 
-pre_init = _signals.signal('pre_init')
-post_init = _signals.signal('post_init')
-pre_save = _signals.signal('pre_save')
-pre_save_post_validation = _signals.signal('pre_save_post_validation')
-pre_post_save_signal = _signals.signal('pre_post_save_signal')
-post_save = _signals.signal('post_save')
-pre_delete = _signals.signal('pre_delete')
-post_delete = _signals.signal('post_delete')
-pre_bulk_insert = _signals.signal('pre_bulk_insert')
-post_bulk_insert = _signals.signal('post_bulk_insert')
+pre_init = _signals.signal("pre_init")
+post_init = _signals.signal("post_init")
+pre_save = _signals.signal("pre_save")
+pre_save_post_validation = _signals.signal("pre_save_post_validation")
+pre_post_save_signal = _signals.signal("pre_post_save_signal")
+post_save = _signals.signal("post_save")
+pre_delete = _signals.signal("pre_delete")
+post_delete = _signals.signal("post_delete")
+pre_bulk_insert = _signals.signal("pre_bulk_insert")
+post_bulk_insert = _signals.signal("post_bulk_insert")
